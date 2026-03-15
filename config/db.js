e6f6c7d412d99130dd87760d5f4a18908fe045c3
NoleMusk <farelvalerian0@gmail.com>
2026-03-15 15:32:06 +0800

    edit db.js

--- config/db.js
+++ config/db.js
@@ -1,10 +1,11 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
- host: "localhost",
- user: "root",
- password: "",
- database: "seattle_search"
+  host: process.env.MYSQLHOST=localhost,
+  user: process.env.MYSQLUSE0R=root,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE=seattle_search,
+  port: process.env.MYSQLPORT=3306
 });
 
 module.exports = db;
