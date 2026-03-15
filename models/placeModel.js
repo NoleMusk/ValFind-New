0e7a821caaa727202f40d0dee245198d32dace2a
NoleMusk <farelvalerian0@gmail.com>
2026-03-15 19:45:28 +0800

    ganti place model

--- models/placeModel.js
+++ models/placeModel.js
@@ -4,28 +4,15 @@ exports.searchPlaces = (keyword, callback) => {
   const q = `%${keyword}%`;
 
   const sql = `
- SELECT *,
- 
- CASE
-  WHEN title LIKE ? THEN 3
-  WHEN description LIKE ? THEN 2
-  WHEN location LIKE ? THEN 1
-  ELSE 0
- END AS score
- 
- FROM places
- 
- WHERE
- title LIKE ?
- OR description LIKE ?
- OR location LIKE ?
- 
- ORDER BY score DESC
- 
- LIMIT 15
- `;
+  SELECT *
+  FROM places
+  WHERE title LIKE ?
+     OR description LIKE ?
+     OR location LIKE ?
+  LIMIT 15
+  `;
 
-  db.query(sql, [q, q, q, q, q, q], callback);
+  db.query(sql, [q, q, q], callback);
 };
 
 exports.autoComplete = (keyword, callback) => {
